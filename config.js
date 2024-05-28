6486ac034380020581557afea31554c775753b7f
MuhammadHasnainRindBaloch <muhammadhasnainrindbaloch54@gmail.com>
2024-05-28 09:11:54 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,28 +4,28 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 //═══════[Required Variables]════════\\
 global.owner = process.env.OWNER_NUMBER.split(",")
-global.mongodb = process.env.MONGODB_URI || ""
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://DARKSHAN:3000@cluster0 .t1wsjlv.mongodb.net/"
 global.port= process.env.PORT || 5000
 global.email = 'textnow1pak@gmail.com'
 global.github = 'https://github.com/naveeddogar/KING-MD'
 global.location = 'pakistan/multan'
 global.gurl = 'https://whatsapp.com/channel/0029Va66s2IJENxvTJjUtM1w' // add your username
-global.sudo = process.env.SUDO || '923096566451'
-global.devs = '923096566451';
+global.sudo = process.env.SUDO || '923066671786'
+global.devs = '923066671786';
 global.website = 'https://king-session.vercel.app' // Hello world
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/a6b9bbde7feaa92c69c7b.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? 'KING-MD' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'Naveed Dogar' : process.env.OWNER_NAME,
-  sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
-  author:  process.env.PACK_AUTHER.split(";")[0] === undefined ? 'Naveed-Dogar' : process.env.PACK_AUTHER.split(";")[0],
+  botname:   process.env.BOT_NAME === undefined ? '𓆩⋆𝐙𝐈𝐃𝐃𝐈 𝐊𝐈𝐍𝐆 𝐁𝐀𝐋𝐎𝐂𝐇⋆𓆪' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? '𓆩⋆𝐙𝐈𝐃𝐃𝐈 𝐊𝐈𝐍𝐆 𝐁𝐀𝐋𝐎𝐂𝐇⋆𓆪' : process.env.OWNER_NAME,
+  sessionName:  process.env.SESSION_ID === undefined ? true : process.env.SESSION_ID,KING-MD;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUUJpelhmeVlqdXN6QzJXdzFKNGV1dlRMUW91OUxNNHdvbmVaeENNbWFHaz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMnpUTFJKZW8weDF0aC9ObmNXWWVvM1F1R2xhcUpjSzFEeDZCeFdhU3N6bz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJpS011T3VlcVBURWYxblg4ZnhmY2tQYk9KMy91QnhCZ3NFMWdZK1dITjFzPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJKeW5WQmR1Sm9nQkFudWYwS292d2MzYVlUQkdzTWtLUmhaTzI1bStvZldnPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjhPQVN4NFNpb3lxVDlkZUdxYWNtWG4vbmpibm44OWhNWWJVdTVVN29mRkE9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjBZS1g0OEZtckNxT0krT2pWZzV0cy94QTNCWTBzVWVMK05LSVBqZWdoQjg9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWUQzN01vcEQ1UmgzeWZCK0grZldrT0piTVpOcmsxdzlsdGVxbmZlZS9rOD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ21NVUI0b0d1VFdmMGJsczQ5V250KzVYaUtiekFpOTJxR2FGRmY4SHpIZz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InFaMzFMRUMyQkdDa0c2WllOaklDclBIZk02NGQ5bUpRcDBjS3RXSWpxQXE4ZWxaTE9hQXhjK21yNUpXcE1ST1llT1hhL25iWW9iYlRnYUhhWEV1OEF3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTUsImFkdlNlY3JldEtleSI6IkQyaEowRnNFT05jaENQSnBuWm1OeDhoZVFaVzU4K01TUkJ2SWhJOGY0Kzg9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6ImFiaEdjU2UxUzA2a1N6R2doVmI1MVEiLCJwaG9uZUlkIjoiZTVmZWQzZGUtZjdkZS00MmYzLTk3ZTgtZjNkMDU0MjQ4NjVlIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlhsMEltdEozRzF3dTlmeGRhbjM3ams0cTNKWT0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJwUkQ5cVRZWDRJTWgrZ1MwNTkzMWdVdHM3VmM9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiWjZCOUFFUzciLCJtZSI6eyJpZCI6IjkyMzA2NjY3MTc4NjoyNUBzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiLwnZCZ8J2QiPCdkIPwnZCD8J2QiCDwnZCK8J2QiPCdkI3wnZCGIPCdkIHwnZCA8J2Qi/CdkI7wnZCC8J2Qh1xuICAgIOKLhlxuICAgIOKLhlxuICAgIOKLhlxuICAgIOKLhlxuICAgIOKLhlxuICAgIOKLhlxuICAgIOKLhlxuICAgIOKLhlxuICDwk4ap4oG34oG44oG28JOGqiJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDUFNEcGRNQ0VNMnYxYklHR0FRZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiV1lGZWx2bmZQaDVXNENteFAzVUJPUzJYZHF3Y0JTQ2hqUzZPSGpOVkYxYz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiZkswT1ZLWlNMVGlIaGZic2piQWhyWEFsNXAvdExLbmdWZ2xuOG1sUTJMbytpSDNhMVlGWHBrMC9PV0F0eE1hNER1bjVMLzBZR3BXUjBZUXR0ZHlyQ0E9PSIsImRldmljZVNpZ25hdHVyZSI6IjArTUpXYU11dFVsNzZHOC8rZkRMQ01DcXdpV2pzTU4yYzExa3hqRG5HSUNNOGt5R1VRbnhNRWg4R3hKQVZKb0ZQQStpY3pVeVI4U3VGVkpSc1h1eUNRPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTIzMDY2NjcxNzg2OjI1QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQlZtQlhwYjUzejRlVnVBcHNUOTFBVGt0bDNhc0hBVWdvWTB1amg0elZSZFgifX1dLCJwbGF0Zm9ybSI6InNtYmEiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MTY4NjkwODIsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBRW14In0=
+  author:  process.env.PACK_AUTHER.split(";")[0] === undefined ? '𓆩⋆𝐙𝐈𝐃𝐃𝐈 𝐊𝐈𝐍𝐆 𝐁𝐀𝐋𝐎𝐂𝐇⋆𓆪' : process.env.PACK_AUTHER.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS  || 'true' ,
-  packname:  process.env.PACK_NAME.split(";")[1] === undefined ? 'King-Md' : process.env.PACK_NAME.split(";")[1],
+  packname:  process.env.PACK_NAME.split(";")[1] === undefined ? '𓆩⋆𝐙𝐈𝐃𝐃𝐈 𝐊𝐈𝐍𝐆 𝐁𝐀𝐋𝐎𝐂𝐇⋆𓆪' : process.env.PACK_NAME.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION ||  'true' ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nigga' : process.env.ANTI_BAD_WORD,
-  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
+  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? true : process.env.ALWAYS_ONLINE,
   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '212' : process.env.FAKE_COUNTRY_CODE,
-  readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
+  readmessage:  process.env.READ_MESSAGE === undefined ? true : process.env.READ_MESSAGE,
   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
   HANDLERS:  process.env.PREFIX === undefined ? '.' : process.env.PREFIX,
   warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
@@ -44,7 +44,7 @@ module.exports = {
     APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
 },
   VERSION: process.env.VERSION === undefined ? 'Ｖ-1.2.9' : process.env.VERSION,
-  LANG: process.env.THEME|| 'KING-MD',
+  LANG: process.env.THEME|| '𓆩⋆𝐙𝐈𝐃𝐃𝐈 𝐊𝐈𝐍𝐆 𝐁𝐀𝐋𝐎𝐂𝐇⋆𓆪',
   WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
 };
 
